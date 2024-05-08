4887bf9b9279ab450cfb0bba2a245e7e020ed845
Ronaldlee88526 <158351628+Ronaldlee88526@users.noreply.github.com>
2024-05-08 11:02:27 -0400

    Ch10

---
+++ analyze apply extend-4/apply/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+  Hung-Ching Lee
+  05/06/24
+  script.js
+*/
+
+// Function to display content
+function content() {
+  var text = document.getElementById("new");
+  text.textContent = "Hi. I am a new paragraph that was created with
+  a JavaScript function.";
+  text.style.color = "#c0145f";
+  text.style.fontSize = "2em";
+}
---
+++ analyze apply extend-4/extend/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+    Student Name: HC Lee
+    File Name: script.js
+    Date: 05/06/24
+*/
+
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $("p").hide();
+  });
+  $(".btn2").click(function(){
+    $("p").show();
+  });
+});
